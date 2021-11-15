4a78ddf67947294a94a5041939f5e31644681137
jvpink200 <jvillarr20@gmail.com>
2021-11-14 20:59:11 -0800

    links added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let inputBtn = document.getElementById("input-btn");
-
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+let ulEl = document.getElementById("ul-el");
+console.log(ulEl);
 inputBtn.addEventListener("click", function () {
-        console.log('btn clicked from event listener')
-    }
-)
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for(let i =0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
