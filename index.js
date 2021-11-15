b3d47db580ab91baf985212daa5dd8f506dd1fcc
jvpink200 <jvillarr20@gmail.com>
2021-11-14 21:47:08 -0800

    changed to use template literals

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el");
 console.log(ulEl);
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = '';
+  renderLeads();
 });
 
-for(let i =0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      `<li>
+        <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}
+        </a>
+      </li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
