f6cc40a32d063ae04ebc6381bf09ac4d1141545b
jvpink200 <jvillarr20@gmail.com>
2021-11-17 18:46:13 -0800

    stored localStorage data in a variable

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el");
 
-console.log(ulEl);
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+  
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
