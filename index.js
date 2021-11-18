c504ad8b2c0863a58913807cc7cd8aeb45a1a62d
jvpink200 <jvillarr20@gmail.com>
2021-11-17 21:49:29 -0800

    refactored render function

--- index.js
+++ index.js
@@ -7,34 +7,35 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
+}
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li>
+        <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
+        </a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
 
 deleteBtn.addEventListener("dblclick", function() {
     console.log("I was dbl clicked!");
     myLeads = [];
     localStorage.clear();
-    renderLeads();
+    render(myLeads);
 })
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
   
   console.log(localStorage.getItem("myLeads"));
 });
 
-function renderLeads() {
-    let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-    listItems +=
-      `<li>
-        <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
-        </a>
-      </li>`;
-    }
-    ulEl.innerHTML = listItems;
-}
+
